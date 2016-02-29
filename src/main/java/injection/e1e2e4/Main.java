b701d9408543edb0f48d36955f58d16236f4968b
Sandra <sandrachacon@alumnos.upm.es>
2016-02-29 18:09:42 +0100

    Creado y subido Ejercicio1 como en clase

---
+++ src/main/java/injection/e1e2e4/Main.java
@@ -0,0 +1,19 @@
+package injection.e1e2e4;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import e1Config.E1Config;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(E1Config.class);
+		MessageManager manager = context.getBean(MessageManager.class);
+		manager.addMessage("1", "uno");
+		System.out.println(manager.findMessage("1"));
+		((AbstractApplicationContext) context).close();
+	}
+
+}
